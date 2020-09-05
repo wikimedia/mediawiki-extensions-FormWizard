0534094ecf71b506a241269d2456003ddcb8f19e
Umherirrender <umherirrender_de.wp@web.de>
2020-09-05 12:45:39 +0200

    Remove hook ResourceLoaderTestModules
    The test file is empty
    
    Change-Id: I7e7d8e02aca6ff25ee8559d3e49a12b3c4128228

--- Hooks.php
+++ Hooks.php
@@ -9,32 +9,6 @@
 class FormWizardHooks {
 
 	/**
-	 * Conditionally register the unit testing module for the ext.formWizard
-	 * module only if that module is loaded.
-	 *
-	 * @param array &$testModules The array of registered test modules
-	 * @param ResourceLoader &$resourceLoader The reference to the resource
-	 *  loader
-	 * @return true
-	 */
-	public static function onResourceLoaderTestModules(
-		array &$testModules,
-		ResourceLoader &$resourceLoader
-	) {
-		$testModules['qunit']['ext.formWizard.tests'] = [
-			'scripts' => [
-				'tests/FormWizard.test.js'
-			],
-			'dependencies' => [
-				'ext.formWizard'
-			],
-			'localBasePath' => __DIR__,
-			'remoteExtPath' => 'FormWizard'
-		];
-		return true;
-	}
-
-	/**
 	 * Hook for BeforePageDisplay.
 	 *
 	 * Enables JavaScript.
